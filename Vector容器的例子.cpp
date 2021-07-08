bca82f945d575b93a9bdb83c1183ff4595b28cce
alatzr <32774513+alatzr@users.noreply.github.com>
2021-07-08 15:16:18 +0800

    Update and rename Vector容器的例子.md to Vector容器的例子.cpp

--- Vector容器的例子.cpp
+++ Vector容器的例子.cpp
@@ -1,6 +1,6 @@
-# Vector 容器的例子
+// Vector 容器的例子
 
-```
+
 # include <iostream>
 # include <vector>
 
@@ -28,9 +28,11 @@ int main() {
 	return 0;
 }
 
-```
+/*
 *输出：*
 
 0|1|2|3|4|5|6|7|8|9|10|
 
 0|1|2|3|4|5|
+
+*/
