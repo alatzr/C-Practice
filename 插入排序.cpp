e1a2196bc98e095ae63b170eca46589928a3b106
alatzr <32774513+alatzr@users.noreply.github.com>
2021-07-08 17:49:53 +0800

    Add files via upload

---
+++ 插入排序.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, i, j, k;
+    int a[110];
+    
+    cin >> n;
+    
+    for (i = 0; i < n; i++) cin >> a[i];
+    
+    for (i = 1; i < n; i++)
+    {   
+        k = a[i];
+        for (j = i-1; j >= 0; j--)
+        {
+            if (k < a[j]) 
+                a[j+1] = a[j];
+            else
+                break;
+        }
+        a[j+1] = k;
+    }
+    
+    for (int i = 0; i < n; i++) cout << a[i] << ' ';
+    
+    return 0;
+}
